d381d3efc09b0a53195d15866a3dc31eb1a158e8
Sethu S <sethu1705sd@gmail.com>
2025-02-03 13:50:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ app.use('/api', userRoutes);
 
 app.listen(5000, ()=> console.log('server running on 5000'))
 
-mongoose.connect('mongodb://localhost:27017/sample')
-    .then(()=> console.log('database connected..'))
-    serverSelectionTimeoutMS: 50000, // 30 seconds
-    .catch((err) => console.log(err))
+mongoose.connect('mongodb://localhost:27017/sample', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 50000, // 30 seconds
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
 
 
 
